799742696490da376ebcec46e6856b86e42c48d8
remanx2558 <43828194+remanx2558@users.noreply.github.com>
2021-01-21 16:34:43 +0530

    Update Wallet.java

--- entity/Wallet.java
+++ entity/Wallet.java
@@ -2,10 +2,12 @@ package com.example.Wallet.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -17,7 +19,10 @@ import lombok.Setter;
 public class Wallet {
 	 @Id
 	 @Column(name="phone")
+	 @NotNull
+	 @Size(min = 2 , message="First Name should have atelast 2 characters")
 	    private Integer phone;
+	 @NotBlank
 	 @Column(name="balance")
 	    private Integer balance;
 
