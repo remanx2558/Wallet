29a4ca21b6165ffa5bb94257896d45582f7116ea
remanx2558 <43828194+remanx2558@users.noreply.github.com>
2021-01-21 16:30:28 +0530

    Update WalletController.java

--- controller/WalletController.java
+++ controller/WalletController.java
@@ -3,12 +3,17 @@ package com.example.Wallet.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Wallet.entity.Wallet;
+import com.example.Wallet.exception.ResourceNotFoundException;
 import com.example.Wallet.repository.WalletRepository;
 import com.example.Wallet.service.WalletService;
 
@@ -32,9 +37,35 @@ public class WalletController {
 
 	    }
 
-	    //for displaying all the wallets present in the database
+	    //1: for displaying all the wallets present in the database
 	    @GetMapping(value = "/wallet/all")
 	    public List<Wallet> displayAll() {
 	        return walletService.getWallets();
 	    }
+	    //2: display specific wallet user
+	    @GetMapping(value = "/wallet/{phone}")
+	    public List<Wallet> displaySpecific(@PathVariable (value = "phone") int phone) {
+	    	List<Wallet> ans=this.walletService.findbyPhone(phone);
+	    	if(ans.size()>0) {
+	    		return ans;
+	    	}
+	    	throw new ResourceNotFoundException("user not Fondd with phone : "+phone);
+	    	
+	        
+	    }
+	    //3:Delete wallet
+	    @DeleteMapping(value = "/wallet/{phone}")
+		public ResponseEntity<Wallet> deleteUser(@PathVariable("phone") int phone){
+			
+	    	List<Wallet> ans=this.walletService.findbyPhone(phone);
+	    	if(ans.size()>0) {
+	    		this.walletService.delete(ans.get(0));
+				return ResponseEntity.ok().build();
+
+	    	}
+	    	throw new ResourceNotFoundException("user not Fondd with phone : "+phone);
+	   }
+	    
+
+	    
 }
