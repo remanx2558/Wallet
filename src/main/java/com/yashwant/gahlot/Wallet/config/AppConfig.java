a16ca6fe59ea3560948923610938b5a107085a98
remanx2558 <gyash2558@gmail.com>
2024-12-07 14:09:20 +0530

    something aws ec2 upload

---
+++ src/main/java/com/yashwant/gahlot/Wallet/config/AppConfig.java
@@ -0,0 +1,8 @@
+package com.yashwant.gahlot.Wallet.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    // Application-level configurations if required
+}
